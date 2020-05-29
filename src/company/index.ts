b60f8f63003b8935918f872d6786db416ca2bc49
Takahito Yamatoya <info@poop.jp>
2020-05-29 22:36:42 +0900

    change js

--- src/company/index.ts
+++ src/company/index.ts
@@ -4,52 +4,76 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 Chart.plugins.unregister(ChartDataLabels);
 
 window.addEventListener('DOMContentLoaded', function () {
-    const graph = new Graph()
+    graph();
 });
 
-class Graph {
-    private ctx = document.getElementById('myChart');
-    constructor() {
-        if (this.ctx instanceof HTMLCanvasElement) {
-            var myChart = new Chart(this.ctx, {
-                type: 'bar',
-                data: {
-                    "labels": ["2018/4-6", "2018/7-9", "2018/10-12", "2019/1-3", "2019/4-6", "2019/7-9", "2019/10-12", "2020/1-3"],
-                    datasets: [{
-                        data: [9.42, 3.15, 5.50, 3.10, 12.75, 8.02, 4.81, 12.57],
-                        "backgroundColor": "rgba(255, 99, 132, 0.2)",
-                        "borderColor": "rgb(255, 99, 132)",
-                        "borderWidth": 1
+interface CompanyData {
+    label: string[];
+    revenue: number[];
+}
+
+async function graph() {
+    const ctx = document.getElementById('myChart');
+
+    const data = await http<CompanyData>(
+        "https://yamatoya.github.io/coincheck/data.json"
+    );
+
+    if (ctx instanceof HTMLCanvasElement) {
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                "labels": data.label,
+                datasets: [{
+                    data: data.revenue,
+                    "backgroundColor": "rgba(255, 99, 132, 0.2)",
+                    "borderColor": "rgb(255, 99, 132)",
+                    "borderWidth": 1
+                }]
+            },
+            plugins: [ChartDataLabels],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
                     }]
                 },
-                plugins: [ChartDataLabels],
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    },
-                    maintainAspectRatio: false,
-                    responsive: false,
-                    legend: {
-                        display: false
-                    },
-                    plugins: {
-                        datalabels: {
-                            color: 'black',
-                            display: true,
-                            formatter: function (value: number) {
-                                return value + '億円'
-                            }
-                        },
-                        font: {
-                            weight: 'bold'
+                maintainAspectRatio: false,
+                responsive: false,
+                legend: {
+                    display: false
+                },
+                plugins: {
+                    datalabels: {
+                        color: 'black',
+                        display: true,
+                        formatter: function (value: number) {
+                            return value + '億円'
                         }
+                    },
+                    font: {
+                        weight: 'bold'
                     }
                 }
-            });
-        }
+            }
+        });
     }
+}
+
+async function http<T>(
+    request: RequestInfo
+): Promise<T> {
+    const response = await fetch(request);
+    const body = await response.json();
+    return body;
+}
+
+// example consuming code
+interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
 }
