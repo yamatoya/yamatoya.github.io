6cceb0abf5f156596f299bff90e3b934e258f575
yamatoya <yamatoya@users.noreply.github.com>
2024-10-19 17:11:59 +0000

    Deploy from source repository

--- webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const merge = require('webpack-merge') // webpack-merge
-const common = require('./webpack.config.js') // 汎用設定をインポート
-
-// common設定とマージする
-module.exports = merge(common, {
-    mode: 'production', // 開発モード
-})
